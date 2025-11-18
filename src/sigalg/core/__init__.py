2b62888ff552b9cfb979bbed2f5a8a1156a33d49
jmyers7 <jmmyers25@gmail.com>
2025-11-18 13:20:55 -0500

    Added FeaturizedSampleSpace and FeaturizedProbabilitySpace. Continued to refactor to fit these new classes.

--- src/sigalg/core/__init__.py
+++ src/sigalg/core/__init__.py
@@ -1,22 +1,22 @@
-from . import feature_representations
-from . import random_objects
-from . import sigma_algebras
-from . import spaces
-from . import probability_measures
-
-from .feature_representations import *
+from . import (
+    featurized_spaces,
+    probability_measures,
+    random_objects,
+    sigma_algebras,
+    spaces,
+)
+from .featurized_spaces import *
+from .probability_measures import *
 from .random_objects import *
 from .sigma_algebras import *
 from .spaces import *
-from .probability_measures import *
-
 
 __all__ = (
-    getattr(feature_representations, "__all__", [])
-    + getattr(random_objects, "__all__", [])
+    getattr(random_objects, "__all__", [])
     + getattr(sigma_algebras, "__all__", [])
     + getattr(spaces, "__all__", [])
     + getattr(probability_measures, "__all__", [])
+    + getattr(featurized_spaces, "__all__", [])
     # + getattr(rvs, "__all__", [])
     # + getattr(operators, "__all__", [])
     # + getattr(probability_measures, "__all__", [])
