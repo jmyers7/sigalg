2aa0a148d2d30c3a2bc4b7044ae8a42ea45d9ec3
jmyers7 <jmmyers25@gmail.com>
2025-12-02 13:56:10 -0500

    Removed `SampleSpaceMethods`.

--- src/sigalg/core/spaces/__init__.py
+++ src/sigalg/core/spaces/__init__.py
@@ -1,12 +1,11 @@
 from .event import Event
 from .event_space import EventSpace
 from .probability_space import ProbabilitySpace
-from .sample_space import SampleSpace, SampleSpaceMethods
+from .sample_space import SampleSpace
 
 __all__ = [
     "SampleSpace",
     "Event",
     "EventSpace",
     "ProbabilitySpace",
-    "SampleSpaceMethods",
 ]
