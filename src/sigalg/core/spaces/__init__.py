751da9667d1b1645a85418e9aeb114a7a5d4f727
jmyers7 <jmmyers25@gmail.com>
2025-12-06 13:02:16 -0500

    Added `SampleSpaceMethods`.

--- src/sigalg/core/spaces/__init__.py
+++ src/sigalg/core/spaces/__init__.py
@@ -1,10 +1,11 @@
 from .event import Event
 from .event_space import EventSpace
 from .probability_space import ProbabilitySpace
-from .sample_space import SampleSpace
+from .sample_space import SampleSpace, SampleSpaceMethods
 
 __all__ = [
     "SampleSpace",
+    "SampleSpaceMethods",
     "Event",
     "EventSpace",
     "ProbabilitySpace",
