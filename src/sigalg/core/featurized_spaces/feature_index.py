b8bf78c96ea10d2f31b792bcfaa6656c8dc36318
jmyers7 <jmmyers25@gmail.com>
2025-12-06 17:17:10 -0500

    Removed `name` parameter, added `values_name` parameter.

--- src/sigalg/core/featurized_spaces/feature_index.py
+++ src/sigalg/core/featurized_spaces/feature_index.py
@@ -9,26 +9,15 @@ class FeatureIndex:
 
     # --------------------- constructor --------------------- #
 
-    def __init__(self, indices: list[Hashable], name: str = "X") -> None:
-        self._validate_parameters(indices, name)
-        self._values = pd.Index(data=indices, name="features")
-        self._name = name
+    def __init__(self, indices: list[Hashable], values_name: str = "feature") -> None:
+        self._validate_parameters(indices, values_name)
+        self._values = pd.Index(data=indices, name=values_name)
 
     # --------------------- properties --------------------- #
 
     @property
     def values(self) -> pd.Index:
         return self._values.copy()
-
-    @property
-    def name(self) -> str:
-        return self._name
-
-    @name.setter
-    def name(self, name: str) -> None:
-        if not isinstance(name, str):
-            raise TypeError("name must be a string.")
-        self._name = name
 
     # --------------------- factory methods --------------------- #
 
@@ -38,14 +27,16 @@ class FeatureIndex:
         initial_index: int = 0,
         size: int = 10,
         prefix: str = "X",
-        name: str = "X",
+        values_name: str = "feature",
     ) -> FeatureIndex:
         if not isinstance(size, int) or size <= 0:
             raise ValueError("'size' must be a positive integer.")
         if not isinstance(initial_index, int):
             raise TypeError("'initial_index' must be an integer.")
-        if not isinstance(name, str):
-            raise TypeError("'name' must be a string.")
+        if not isinstance(values_name, str):
+            raise TypeError("'values_name' must be a string.")
+        if not isinstance(prefix, str):
+            raise TypeError("'prefix' must be a string.")
 
         if size == 1:
             indices = [prefix]
@@ -53,7 +44,7 @@ class FeatureIndex:
             indices = [
                 f"{prefix}{i}" for i in range(initial_index, initial_index + size)
             ]
-        return cls(indices=indices, name=name)
+        return cls(indices=indices, values_name=values_name)
 
     # --------------------- sequence methods --------------------- #
 
@@ -63,30 +54,21 @@ class FeatureIndex:
     def __iter__(self) -> iter:
         return iter(self._values)
 
-    # --------------------- representation --------------------- #
-
-    def __repr__(self) -> str:
-        return f"Feature index {self.name}:\n{self._values.to_list()}"
-
     # --------------------- equality --------------------- #
 
     def __eq__(self, other: FeatureIndex) -> bool:
-        return (
-            isinstance(other, FeatureIndex)
-            and self.values.equals(other.values)
-            and self.name == other.name
-        )
+        return isinstance(other, FeatureIndex) and self.values.equals(other.values)
 
     # --------------------- validation methods --------------------- #
 
-    def _validate_parameters(self, indices: list[Hashable], name: str) -> None:
+    def _validate_parameters(self, indices: list[Hashable], values_name: str) -> None:
         if not isinstance(indices, list) or not all(
             isinstance(idx, Hashable) for idx in indices
         ):
             raise TypeError("indices must be provided as a list.")
+        if not isinstance(values_name, str):
+            raise TypeError("values_name must be a string.")
         if len(indices) == 0:
             raise ValueError("indices list cannot be empty.")
         if len(indices) != len(set(indices)):
             raise ValueError("indices must be unique (no duplicates allowed).")
-        if not isinstance(name, str):
-            raise TypeError("name must be a string.")
