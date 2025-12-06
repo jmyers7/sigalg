13e028000afe186712d8f7804575e8a26fb3f72c
jmyers7 <jmmyers25@gmail.com>
2025-12-06 17:12:15 -0500

    Added `FeatureIndex` to inheritance.

--- src/sigalg/processes/base/time.py
+++ src/sigalg/processes/base/time.py
@@ -1,30 +1,19 @@
 from __future__ import annotations
 
+from collections.abc import Hashable
 from numbers import Real
 
 import numpy as np
-import pandas as pd
+
+from ...core import FeatureIndex
 
 
-class Time:
+class Time(FeatureIndex):
 
     # --------------------- constructor --------------------- #
 
-    def __init__(self, idx: pd.Index, discrete: bool) -> None:
-        self._values = idx
-        self._discrete = discrete
-
-    # --------------------- properties --------------------- #
-
-    @property
-    def values(self) -> pd.Index:
-        return self._values
-
-    def __iter__(self):
-        return iter(self._values)
-
-    def __len__(self) -> int:
-        return len(self._values)
+    def __init__(self, indices: list[Hashable], values_name: str = "time") -> None:
+        super().__init__(indices=indices, values_name=values_name)
 
     # --------------------- factory methods --------------------- #
 
@@ -34,8 +23,8 @@ class Time:
             raise ValueError("length must be a positive integer.")
         if not isinstance(start, int):
             raise TypeError("start must be an integer.")
-        idx = pd.Index(range(start, start + length), name="time")
-        return cls(idx=idx, discrete=True)
+        indices = list(range(start, start + length))
+        return cls(indices=indices)
 
     @classmethod
     def continuous(cls, start: Real = 0.0, stop: Real = 1.0, step: Real = 0.1) -> Time:
@@ -48,8 +37,5 @@ class Time:
         if stop <= start:
             raise ValueError("stop must be greater than start.")
         num_points = int((stop - start) / step) + 1
-        idx = pd.Index(
-            np.linspace(start, stop, num_points),
-            name="time",
-        )
-        return cls(idx=idx, discrete=False)
+        indices = list(np.linspace(start, stop, num_points))
+        return cls(indices=indices)
