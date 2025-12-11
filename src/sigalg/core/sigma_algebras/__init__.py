f76fe1399c26981165a249342ef87b2c9a084761
jmyers7 <jmmyers25@gmail.com>
2025-12-11 10:59:59 -0500

    Renamed `meet` to `join`, added `FilteredSigmaAlgebra`.

--- src/sigalg/core/sigma_algebras/__init__.py
+++ src/sigalg/core/sigma_algebras/__init__.py
@@ -1,6 +1,7 @@
 from .comparison import is_refinement, is_subalgebra
+from .filtered_sigma_algebra import FilteredSigmaAlgebra
 from .filtration import Filtration
-from .lattice_operations import meet
+from .lattice_operations import join
 from .sigma_algebra import SigmaAlgebra, SigmaAlgebraMethods
 
 __all__ = [
@@ -8,6 +9,7 @@ __all__ = [
     "SigmaAlgebraMethods",
     "is_subalgebra",
     "is_refinement",
-    "meet",
+    "join",
     "Filtration",
+    "FilteredSigmaAlgebra",
 ]
