4e55b45e8295715f6cbe39cecff51832a1575c13
jmyers7 <jmmyers25@gmail.com>
2025-12-10 22:07:06 -0500

    Add `info` sub-package.

--- src/sigalg/core/__init__.py
+++ src/sigalg/core/__init__.py
@@ -1,12 +1,14 @@
 from . import (
     base,
     featurized_spaces,
+    info,
     probability_measures,
     random_objects,
     sigma_algebras,
 )
 from .base import *  # noqa: F403
 from .featurized_spaces import *  # noqa: F403
+from .info import *  # noqa: F403
 from .probability_measures import *  # noqa: F403
 from .random_objects import *  # noqa: F403
 from .sigma_algebras import *  # noqa: F403
@@ -15,6 +17,7 @@ __all__ = (
     getattr(random_objects, "__all__", [])
     + getattr(sigma_algebras, "__all__", [])
     + getattr(base, "__all__", [])
+    + getattr(info, "__all__", [])
     + getattr(probability_measures, "__all__", [])
     + getattr(featurized_spaces, "__all__", [])
 )
