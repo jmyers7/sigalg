66ce57c820d5f925e60a7243e90758aae1dd8259
jmyers7 <jmmyers25@gmail.com>
2025-12-10 22:08:10 -0500

    Added `meet` module-level function.

---
+++ src/sigalg/core/sigma_algebras/lattice_operations.py
@@ -0,0 +1,34 @@
+from typing import TYPE_CHECKING
+
+import pandas as pd
+
+if TYPE_CHECKING:
+    from .sigma_algebra import SigmaAlgebra
+
+
+def meet(sigma_algebras: list[SigmaAlgebra]) -> SigmaAlgebra:
+    from .sigma_algebra import SigmaAlgebra
+
+    if not isinstance(sigma_algebras, list):
+        raise TypeError("Expected a list of SigmaAlgebra instances")
+    if not all(isinstance(alg, SigmaAlgebra) for alg in sigma_algebras):
+        raise TypeError("All elements of the list must be SigmaAlgebra instances")
+    if len(sigma_algebras) <= 0:
+        raise ValueError("The list of SigmaAlgebra instances cannot be empty")
+    if len(sigma_algebras) == 1:
+        return sigma_algebras[0]
+    sample_space = sigma_algebras[0].sample_space
+    if not all(alg.sample_space == sample_space for alg in sigma_algebras):
+        raise ValueError("All SigmaAlgebra instances must have the same sample space")
+
+    df = pd.concat([alg.values for alg in sigma_algebras], axis=1)
+    sample_id_to_atom_id = {}
+    for id, (_, grp) in enumerate(df.groupby(list(df.columns))):
+        atom = grp.index.to_list()
+        for sample_id in atom:
+            sample_id_to_atom_id[sample_id] = id
+    return SigmaAlgebra(
+        sample_id_to_atom_id=sample_id_to_atom_id,
+        sample_space=sample_space,
+        name="meet",
+    )
