7bffce555b409fe7f8c38e3f2ad0ea252bc26768
jmyers7 <jmmyers25@gmail.com>
2025-12-21 13:28:39 -0500

    Removed `FeatureIndex` and `random_variable_range`.

--- src/sigalg/core/base/__init__.py
+++ src/sigalg/core/base/__init__.py
@@ -1,6 +1,5 @@
-from .event import Event
+from .event import Event  # noqa: D104
 from .event_space import EventSpace
-from .feature_index import FeatureIndex
 from .index import Index
 from .probability_space import ProbabilitySpace
 from .sample_space import SampleSpace
@@ -9,7 +8,6 @@ from .time import Time
 __all__ = [
     "Event",
     "EventSpace",
-    "FeatureIndex",
     "Index",
     "ProbabilitySpace",
     "SampleSpace",
