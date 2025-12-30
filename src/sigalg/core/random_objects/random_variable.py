f25d481532f8a3d7b46a55ad9aeb8209c5c8a998
jmyers7 <jmmyers25@gmail.com>
2025-12-30 13:07:25 -0500

    Added an updated `__repr__` method.

--- src/sigalg/core/random_objects/random_variable.py
+++ src/sigalg/core/random_objects/random_variable.py
@@ -7,6 +7,8 @@ from .random_vector import RandomVector
 class RandomVariable(RandomVector):
     """A class representing a random variable, which is a 1-dimensional random vector."""
 
+    # --------------------- constructor --------------------- #
+
     def __init__(
         self,
         outputs: Mapping[Hashable, Hashable],
@@ -16,3 +18,20 @@ class RandomVariable(RandomVector):
         super().__init__(outputs=outputs, domain=domain, name=name)
         if self.dimension != 1:
             raise ValueError("RandomVariable must be 1-dimensional.")
+
+    # --------------------- Representation --------------------- #
+
+    def __repr__(self) -> str:
+        """Get the string representation of the random variable.
+
+        Returns
+        -------
+        repr_str : str
+            The string representation of the random variable.
+        """
+        if self.dimension == 1:
+            data = self.data.to_frame()
+            data.columns = [self.name]
+        else:
+            data = self.data
+        return f"Random variable '{self.name}':\n{data}"
