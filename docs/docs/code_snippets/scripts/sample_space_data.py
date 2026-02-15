a11678c9f8473412bb65ad747f02086566e061d9
jmyers7 <jmmyers25@gmail.com>
2026-02-15 09:57:36 -0500

    In `docs`: updated code snippets for `SampleSpace` [skip ci]

--- docs/docs/code_snippets/scripts/sample_space_data.py
+++ docs/docs/code_snippets/scripts/sample_space_data.py
@@ -2,7 +2,7 @@
 
 from sigalg.core import SampleSpace
 
-# Create a sample space Omega=[s_0, s_1, s_2, s_3, s_4]
+# Create a sample space Omega = [s_0, s_1, s_2, s_3, s_4]
 Omega = SampleSpace().from_sequence(size=5, prefix="s")
 
 # Access the underlying data of the sample space as a pd.Index
