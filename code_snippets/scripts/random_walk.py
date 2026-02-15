28f5ec507c5bf265ce35fbcee79a3c906728144a
jmyers7 <jmmyers25@gmail.com>
2026-02-15 13:01:02 -0500

    Deployed 769bd27 with MkDocs version: 1.6.1

--- code_snippets/scripts/random_walk.py
+++ code_snippets/scripts/random_walk.py
@@ -6,28 +6,19 @@ from scipy.stats import bernoulli
 from sigalg.core import Time
 from sigalg.processes import IIDProcess
 
-# Create an IID Bernoulli process B with p=0.7
-T = Time.discrete(start=1, stop=4)
+T = Time.discrete(start=1, stop=4)  # (1)!
 B = IIDProcess(
     distribution=bernoulli(p=0.7),
     support=[0, 1],
     time=T,
-).from_enumeration()
+).from_enumeration()  # (2)!
 
-# Create a process Y with Y_t = -1 or Y_t = 1
-Y = 2 * B - 1
+Y = 2 * B - 1  # (3)!
+X = Y.cumsum(name="X")  # (4)!
+X.insert_rv(state=0, time=0, in_place=True)  # (5)!
 
-# Create a random walk process X
-X = Y.cumsum(name="X")
+F = X.natural_filtration  # (6)!
+expectation = X[4].expectation(sigma_algebra=F[3])  # (7)!
 
-# Get the natural filtration F_t = σ(X_1,X_2,...,X_t)
-F = X.natural_filtration
-
-# Compute the conditional expectation E(X_4 | F_3)
-expectation = X[4].expectation(sigma_algebra=F[3])
-
-# Print the trajectories of X and the expectation together
-print(pd.concat([X.data, expectation.data], axis=1))
-
-# A random walk with positive drift is a submartingale
-print("\nIs X a submartingale?", X.is_submartingale())
+print(pd.concat([X.data, expectation.data], axis=1))  # (8)!
+print("\nIs X a submartingale?", X.is_submartingale())  # (9)!
