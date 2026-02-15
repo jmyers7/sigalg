0f371355959080282179b30b4109c60db84cdacd
jmyers7 <jmmyers25@gmail.com>
2026-02-15 09:53:22 -0500

    Deployed 3deb398 with MkDocs version: 1.6.1

---
+++ code_snippets/scripts/create_prob_space.py
@@ -0,0 +1,26 @@
+from sigalg.core import ProbabilityMeasure, SampleSpace, SigmaAlgebra
+
+Omega = SampleSpace().from_sequence(size=4)
+
+# Define a sigma-algebra
+atom_ids = {
+    0: 0,
+    1: 1,
+    2: 0,
+    3: 1,
+}
+F = SigmaAlgebra(sample_space=Omega).from_dict(atom_ids)
+
+# Define a probability measure
+probabilities = {
+    0: 0.1,
+    1: 0.2,
+    2: 0.4,
+    3: 0.3,
+}
+P = ProbabilityMeasure(sample_space=Omega).from_dict(probabilities)
+
+# Create a probability space
+prob_space = Omega.make_probability_space(sigma_algebra=F, probability_measure=P)
+
+print(prob_space)
