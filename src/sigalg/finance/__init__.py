da57241abcc27a842aa682ec882e10f42285d804
jmyers7 <jmmyers25@gmail.com>
2026-03-08 11:17:31 -0400

    In `finance` module: add `discount` module-level function [skip ci]

--- src/sigalg/finance/__init__.py
+++ src/sigalg/finance/__init__.py
@@ -1,8 +1,9 @@
 """Module containing components for financial mathematics, including derivative pricing models."""
 
-from .pricing import BinomialPricingModel, european_option
+from .pricing import BinomialPricingModel, discount, european_option
 
 __all__ = [
     "BinomialPricingModel",
     "european_option",
+    "discount",
 ]
--- src/sigalg/finance/pricing/__init__.py
+++ src/sigalg/finance/pricing/__init__.py
@@ -2,5 +2,6 @@
 
 from .binomial_pricing_model import BinomialPricingModel
 from .claims import european_option
+from .transforms import discount
 
-__all__ = ["BinomialPricingModel", "european_option"]
+__all__ = ["BinomialPricingModel", "european_option", "discount"]
