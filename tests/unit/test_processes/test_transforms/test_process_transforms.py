e7dca4dad6a8ee5568f554d928524f282900ae97
jmyers7 <jmmyers25@gmail.com>
2026-03-10 10:49:29 -0400

    In `test_process_transforms.py` testing suite: added unit test for Fundamental Theorem of Calculus

---
+++ tests/unit/test_processes/test_transforms/test_process_transforms.py
@@ -0,0 +1,14 @@
+from sigalg.core import Time
+from sigalg.processes import RandomWalk, StochasticProcess
+
+
+class TestItoIntegral:
+    def test_fundamental_theorem_of_calculus(self):
+        """Integrating the increments of a process against time should return the original process."""
+        time = Time().discrete(length=2)
+        X = RandomWalk(p=0.6, time=time, initial_state=1).from_enumeration()
+        T = StochasticProcess.from_time(domain=X.domain, time=time)
+        integral = X.increments().ito_integral(T)
+
+        assert X[0] + integral == X
+        assert integral.name == "int X_increments dT"
