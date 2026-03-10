a44f445c437ad8d8f313043ab25c2209a022aada
jmyers7 <jmmyers25@gmail.com>
2026-03-10 14:57:15 -0400

    Deployed 790bdf0 with MkDocs version: 1.6.1

---
+++ code_snippets/scripts/binomial_pricing_model.py
@@ -0,0 +1,23 @@
+from sigalg.finance import BinomialPricingModel, european_option
+
+S_0 = 100  # (1)!
+u = 1.1  # (2)!
+r = 0.01  # (3)!
+model = BinomialPricingModel(
+    initial_price=S_0, up_factor=u, risk_free_rate=r, length=3
+)  # (4)!
+
+S = model.price_process  # (5)!
+print("Price process of the stock:\n", S)
+
+call = european_option(price=S[3], strike=100, option_type="call")  # (6)!
+B, N, V, price = model.replicating_portfolio(claim=call)  # (7)!
+
+print("\nThe bank account balance:\n", B)  # (8)!
+print("\nNumber of units of stock held:\n", N)  # (9)!
+print("\nValue of the replicating portfolio:\n", V)  # (10)!
+print("\nFair price of the European call option:\n", price)  # (11)!
+
+F = S.natural_filtration  # (12)!
+print("\nIs the B process predictable:", B.is_predictable(F))
+print("\nIs the N process predictable:", N.is_predictable(F))
--- code_snippets/scripts/gambling_strategy.py
+++ code_snippets/scripts/gambling_strategy.py
@@ -8,20 +8,20 @@ T = Time.discrete(start=0, stop=3)  # (1)!
 Y = RandomWalk(p=0.4, time=T, name="Y").from_enumeration()  # (2)!
 
 
-def f1(Y: StochasticProcess) -> RandomVariable:  # (3)!
+def f0(Y: StochasticProcess) -> RandomVariable:  # (3)!
     return RandomVariable(domain=Y.domain).from_constant(1)
 
 
-def f2(Y: StochasticProcess) -> RandomVariable:  # (4)!
+def f1(Y: StochasticProcess) -> RandomVariable:  # (4)!
     return 2 * (Y[1] > Y[0])
 
 
-def f3(Y: StochasticProcess) -> RandomVariable:  # (5)!
+def f2(Y: StochasticProcess) -> RandomVariable:  # (5)!
     return 2 * (Y[2] > Y[1]) + 1 * (Y[1] > Y[0])
 
 
 X = ProcessTransforms.transform(  # (6)!
-    process=Y, functions=[f1, f2, f3], time=T[1:]
+    process=Y, functions=[f0, f1, f2], time=T[:-1]
 ).with_name("X")
 
 winnings = X.ito_integral(Y)  # (7)!
@@ -31,11 +31,11 @@ print("Is the game unfair to the bettor?", Y.is_supermartingale())  # (9)!
 print("\nWhich games are winners and which are losers?\n", Y.increments())  # (10)!
 print("\nBettor's strategy:\n", X)  # (11)!
 print(
-    "\nIs the bettor's strategy predictable?", X.is_predictable(Y.natural_filtration)
+    "\nIs the bettor's strategy adapted?", X.is_adapted(Y.natural_filtration)
 )  # (12)!
 print("\nBettor's winnings:\n", winnings)  # (13)!
 print(
-    "\nIs the bettor's strategy a winning strategy?",
-    winnings.is_supermartingale(),
+    "\nIs the bettor's strategy a losing strategy?",
+    winnings.is_supermartingale(Y.natural_filtration),
 )  # (14)!
 print("\nExpected winnings:", f"{expected_winnings:0.2f}")  # (15)!
