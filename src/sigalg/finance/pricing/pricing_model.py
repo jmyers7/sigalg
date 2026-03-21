078fa7207a77b57a7a748c985fe5d883e801a814
jmyers7 <jmmyers25@gmail.com>
2026-03-21 15:53:30 -0400

    In `PricingModel` class: removed `replicating_portfolio` abstract method [skip ci]

--- src/sigalg/finance/pricing/pricing_model.py
+++ src/sigalg/finance/pricing/pricing_model.py
@@ -2,26 +2,12 @@
 
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
-from numbers import Real
-from typing import TYPE_CHECKING
-
 from ...processes.base.stochastic_process import StochasticProcess
 
-if TYPE_CHECKING:
-    from ..claims.claim import Claim
 
-
-class PricingModel(ABC, StochasticProcess):
-    """Abstract base class for various types of pricing models."""
+class PricingModel(StochasticProcess):
+    """Abstract base class for pricing models."""
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.enum_mode: str | None = None
-
-    @abstractmethod
-    def replicating_portfolio(
-        self, claim: Claim
-    ) -> tuple[StochasticProcess, StochasticProcess, StochasticProcess, Real]:
-        """Compute the replicating portfolio for a given contingent claim relative to this pricing model."""
-        pass
