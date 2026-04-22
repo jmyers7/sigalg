23e533e144dfa1dfd551717b6bcb5f0d83dcb99c
jmyers7 <jmmyers25@gmail.com>
2026-04-22 12:46:36 -0400

    In `sigma_algebras` module: removed `FilteredSigmaAlgebra` class [skip ci]

--- src/sigalg/core/sigma_algebras/__init__.py
+++ src/sigalg/core/sigma_algebras/__init__.py
@@ -1,11 +1,9 @@
 from .comparison import is_refinement, is_subalgebra, join  # noqa: D104
-from .filtered_sigma_algebra import FilteredSigmaAlgebra
 from .filtration import Filtration
 from .sigma_algebra import SigmaAlgebra
 
 __all__ = [
     "SigmaAlgebra",
-    "FilteredSigmaAlgebra",
     "Filtration",
     "is_refinement",
     "is_subalgebra",
