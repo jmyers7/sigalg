06f2a7396df77b54cee65fe3731a70bb4516ea69
jmyers7 <jmmyers25@gmail.com>
2026-05-27 12:45:50 -0400

    In `Domain` class: added a `__repr__` method [skip ci]

--- src/sigalg/core/base/domain.py
+++ src/sigalg/core/base/domain.py
@@ -10,3 +10,22 @@ class Domain(Index):
 
     def __init__(self, name: Hashable = "D") -> None:
         super().__init__(name=name)
+
+    def __repr__(self) -> str:
+        """Return a string representation of the domain.
+
+        Returns
+        -------
+        repr_str : str
+            String representation of the domain.
+        """
+        if self.data is None:
+            if self.name is None:
+                return "Domain: empty"
+            else:
+                return f"Domain '{self.name}': empty"
+        else:
+            if self.name is None:
+                return f"Domain:\n{self.data.to_list()}"
+            else:
+                return f"Domain '{self.name}':\n{self.data.to_list()}"
