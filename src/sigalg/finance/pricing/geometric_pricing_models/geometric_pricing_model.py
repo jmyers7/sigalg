1e7bdc5d1d7e82eff2f33bab6559c06dfe836410
jmyers7 <jmmyers25@gmail.com>
2026-07-07 00:04:45 +0200

    In `GeometricPricingModel` class: removed stale method `from_price_data` [skip ci]

--- src/sigalg/finance/pricing/geometric_pricing_models/geometric_pricing_model.py
+++ src/sigalg/finance/pricing/geometric_pricing_models/geometric_pricing_model.py
@@ -3,10 +3,8 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from collections.abc import Hashable
 from numbers import Real
 
-from ....core.base.time import Time
 from ....core.probability_measures.parametrized_probability_measure import (
     ParametrizedProbabilityMeasure,
 )
@@ -22,51 +20,6 @@ class GeometricPricingModel(ABC, StochasticProcess):
         "_driving_process",
         "_emms",
     ]
-    _repr_name = "Geometric price process"
-
-    # --------------------- constructors --------------------- #
-
-    @classmethod
-    def from_price_data(
-        cls,
-        initial_price: Real,
-        risk_free_rate: Real,
-        index: Time | None = None,
-        name: Hashable = "S",
-    ):
-        """Initialize a geometric pricing model from basic pricing data.
-
-        Parameters
-        ----------
-        initial_price : Real
-            The initial price of the underlying asset.
-        risk_free_rate : Real
-            The risk-free rate of return.
-        time : Time | None, default=None
-            The time index of the model. If `None`, a time index will be generated later through data generation methods.
-        name : Hashable | None, default="S"
-            The name of the model.
-
-        Raises
-        ------
-        TypeError
-            If `initial_price` or `risk_free_rate` is not a real number.
-        ValueError
-            If `initial_price` or `risk_free_rate` is not positive.
-        """
-        if not isinstance(initial_price, Real):
-            raise TypeError("initial_price must be a real number.")
-        if not isinstance(risk_free_rate, Real):
-            raise TypeError("risk_free_rate must be a real number.")
-        if initial_price <= 0:
-            raise ValueError("initial_price must be positive.")
-        if risk_free_rate <= 0:
-            raise ValueError("risk_free_rate must be positive.")
-
-        process = cls(index=index, name=name)
-        process.initial_price = initial_price
-        process.risk_free_rate = risk_free_rate
-        process.risk_free_gross_return = 1 + risk_free_rate
 
     # --------------------- data generation methods --------------------- #
 
